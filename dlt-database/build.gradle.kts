973e42bbe3e078b08c4c605fa003cd7ab047002d
Florian Roks <froks@users.noreply.github.com>
2024-04-11 03:37:48 +0200

    use streams instead of callbacks

--- dlt-database/build.gradle.kts
+++ dlt-database/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.12")
     implementation("org.xerial:sqlite-jdbc:3.45.2.0")
     implementation("com.zaxxer:HikariCP:5.1.0")
+    api("org.ktorm:ktorm-core:3.6.0")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
